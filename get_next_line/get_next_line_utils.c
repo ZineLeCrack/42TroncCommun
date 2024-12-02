04d5523d37391893989e86908cb202cf64f37e69
ZineLeCrack <rlebaillif27@gmail.com>
2024-12-02 12:37:14 +0100

    Add files via upload

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlebaill <rlebaill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 17:03:45 by rlebaill          #+#    #+#             */
+/*   Updated: 2024/11/05 18:27:06 by rlebaill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	str = malloc((ft_strlen((char *)s1) + ft_strlen((char *)s2)) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = malloc(sizeof(char) * ft_strlen(src) + 1);
+	if (dest == NULL)
+		return (NULL);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+void	ft_strcpy(char *buff, char *new_buff)
+{
+	int	i;
+
+	if (!new_buff)
+		return ;
+	i = 0;
+	while (i < BUFFER_SIZE)
+	{
+		buff[i] = '\0';
+		i++;
+	}
+	i = 0;
+	while (new_buff[i] && i < BUFFER_SIZE)
+	{
+		buff[i] = new_buff[i];
+		i++;
+	}
+}
